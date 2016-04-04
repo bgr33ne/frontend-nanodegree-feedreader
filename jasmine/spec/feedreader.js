b28431d9158f6273354997646b3d98c0a4488333
Brian Greene <bgreene623@gmail.com>
2016-04-04 14:14:22 -0400

    added constructors for next tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -92,6 +92,8 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
+
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -99,11 +101,23 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it('loadfeed creates at least one entry element in feed container', function() {
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         });
+
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         it('new feed loaded has content that changes', function() {
+
+         });
+
+    });
 }());
